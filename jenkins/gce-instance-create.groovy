652cbbcdb531744d099c576d2e60e84ef0983bdb
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2023-01-11 07:45:14 +0530

    initial commit

---
+++ jenkins/gce-instance-create.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    environment {
+    SVC_ACCOUNT_KEY = credentials('terraform-auth')
+  }
+     
+    stages {
+      	stage('Set creds') {
+            steps {
+              
+                sh 'echo $SVC_ACCOUNT_KEY | base64 -d > ./jenkins/jenkins.json'
+		            sh 'pwd' 
+               
+            }
+        }
+
+	
+	stage('Auth-Project') {
+	 steps {
+		 dir('module-3')
+		 {
+    
+        sh 'gcloud auth activate-service-account jenkins@mi-dev-lab.iam.gserviceaccount.com --key-file=jenkins.json'
+    }
+    }
+	}
+ 	 
+	stage('Create Instance') {
+	 steps {
+    
+    sh 'gcloud compute instances create $VM_NAME --zone=$ZONES --tags=http-server'
+        
+    }
+    }
+     
+   }
+}
